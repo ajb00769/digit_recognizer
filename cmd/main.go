da0b88f983bbb36a92f3f75c8e9a9fb8ca29a842
Allen Bercero <allenbercero@gmail.com>
2026-02-02 23:35:21 +0800

    test(input): add partial tests and main cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,3 @@
 package main
 
-import (
-	"allenbercero/digit_recognizer/ml"
-	"fmt"
-	"os"
-)
-
-func main() {
-	matrixA := [][]float64{
-		{2, -1, 3},
-		{0, 4, -2},
-	}
-
-	matrixB := [][]float64{
-		{1, 5},
-		{-3, 2},
-		{4, -1},
-	}
-
-	result, err := ml.MatMul(matrixA, matrixB)
-
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("%v\n", result)
-}
+func main() {}
---
+++ ml/neural_net_test.go
@@ -0,0 +1,35 @@
+package ml
+
+import (
+	"os"
+	"testing"
+)
+
+var testMatrix [28][28]float64
+var emptyMatrix [28][28]float64
+var emptyArray [784]float64
+
+func populateTestMatrix() {
+	for i := range testMatrix {
+		for j := range testMatrix[i] {
+			testMatrix[i][j] = ((float64(i) * 28.0) + float64(j)) / 100.0
+		}
+	}
+}
+
+func populateExpectedInputHappyPath(arr *[784]float64) {}
+
+func TestMain(m *testing.M) {
+	populateTestMatrix()
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
+
+func TestInputHappyPath(t *testing.T) {}
+func TestInputAllZeroes(t *testing.T) {
+	result := input(&emptyMatrix)
+	if result != emptyArray {
+		t.Errorf("got %v, want %v", result, emptyArray)
+	}
+}
+func TestInputFlatOrder(t *testing.T) {}
