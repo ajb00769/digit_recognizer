bad42fff8445842a83214bb8cd5fd520e759f561
Allen Bercero <allenbercero@gmail.com>
2026-03-16 23:42:10 +0800

    feat(main): initial main function just to test out training with arbitrary hyper parameters

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"allenbercero/digit_recognizer/train"
+	"fmt"
+)
+
+func main() {
+	trainingFile := "files/mnist_train.csv"
+	testingFile := "files/mnist_test.csv"
+
+	layerCount := uint(2)
+	neuronCount := uint(16)
+	weightCount := uint(784)
+
+	fmt.Println("Starting training...")
+	train.Train(trainingFile, testingFile, layerCount, neuronCount, weightCount)
+	fmt.Println("Training complete.")
+}
